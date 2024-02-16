8b79b862e715252a6df6aec15e06cbef12073e14
stepien20 <102522088+stepien20@users.noreply.github.com>
2024-02-16 12:59:42 +0100

    Add files via upload

---
+++ tworzenie_bazy_dancyh/lekcja1/sklep_elektroniczny2.sql
@@ -0,0 +1,209 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 16 Lut 2024, 12:57
+-- Wersja serwera: 10.4.27-MariaDB
+-- Wersja PHP: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `sklep_elektroniczny2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `dostawcy`
+--
+
+CREATE TABLE `dostawcy` (
+  `id_dostawcy` int(11) NOT NULL,
+  `nazwa` enum('Pocztex','InPost','GLS') NOT NULL,
+  `koszt_dostawy` float(2,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `kategorie`
+--
+
+CREATE TABLE `kategorie` (
+  `id_kategorii` int(11) NOT NULL,
+  `nazwa_kategorii` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `klienci`
+--
+
+CREATE TABLE `klienci` (
+  `id_klienta` int(11) NOT NULL,
+  `miasto` varchar(120) NOT NULL,
+  `adres` varchar(120) NOT NULL,
+  `nazwa` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `szczegoly_zamowienia`
+--
+
+CREATE TABLE `szczegoly_zamowienia` (
+  `id_zamowienia` int(11) NOT NULL,
+  `id_towaru` int(11) NOT NULL,
+  `ilosc` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `towary`
+--
+
+CREATE TABLE `towary` (
+  `id_towaru` int(11) NOT NULL,
+  `id_kategorii` int(11) NOT NULL,
+  `nazwa` varchar(120) NOT NULL,
+  `cena` float(5,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `zamowienia`
+--
+
+CREATE TABLE `zamowienia` (
+  `id_zamowienia` int(11) NOT NULL,
+  `data_zalozenia` date NOT NULL,
+  `id_klienta` int(11) NOT NULL,
+  `id_dostawcy` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `dostawcy`
+--
+ALTER TABLE `dostawcy`
+  ADD PRIMARY KEY (`id_dostawcy`);
+
+--
+-- Indeksy dla tabeli `kategorie`
+--
+ALTER TABLE `kategorie`
+  ADD PRIMARY KEY (`id_kategorii`);
+
+--
+-- Indeksy dla tabeli `klienci`
+--
+ALTER TABLE `klienci`
+  ADD PRIMARY KEY (`id_klienta`);
+
+--
+-- Indeksy dla tabeli `szczegoly_zamowienia`
+--
+ALTER TABLE `szczegoly_zamowienia`
+  ADD PRIMARY KEY (`id_zamowienia`),
+  ADD KEY `id_towaru` (`id_towaru`);
+
+--
+-- Indeksy dla tabeli `towary`
+--
+ALTER TABLE `towary`
+  ADD PRIMARY KEY (`id_towaru`),
+  ADD KEY `id_kategorii` (`id_kategorii`);
+
+--
+-- Indeksy dla tabeli `zamowienia`
+--
+ALTER TABLE `zamowienia`
+  ADD PRIMARY KEY (`id_zamowienia`),
+  ADD KEY `id_dostawcy` (`id_dostawcy`),
+  ADD KEY `id_klienta` (`id_klienta`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `dostawcy`
+--
+ALTER TABLE `dostawcy`
+  MODIFY `id_dostawcy` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `kategorie`
+--
+ALTER TABLE `kategorie`
+  MODIFY `id_kategorii` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `klienci`
+--
+ALTER TABLE `klienci`
+  MODIFY `id_klienta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `szczegoly_zamowienia`
+--
+ALTER TABLE `szczegoly_zamowienia`
+  MODIFY `id_zamowienia` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `towary`
+--
+ALTER TABLE `towary`
+  MODIFY `id_towaru` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `zamowienia`
+--
+ALTER TABLE `zamowienia`
+  MODIFY `id_zamowienia` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `szczegoly_zamowienia`
+--
+ALTER TABLE `szczegoly_zamowienia`
+  ADD CONSTRAINT `szczegoly_zamowienia_ibfk_1` FOREIGN KEY (`id_zamowienia`) REFERENCES `zamowienia` (`id_zamowienia`),
+  ADD CONSTRAINT `szczegoly_zamowienia_ibfk_2` FOREIGN KEY (`id_towaru`) REFERENCES `towary` (`id_towaru`);
+
+--
+-- Ograniczenia dla tabeli `towary`
+--
+ALTER TABLE `towary`
+  ADD CONSTRAINT `towary_ibfk_1` FOREIGN KEY (`id_kategorii`) REFERENCES `kategorie` (`id_kategorii`);
+
+--
+-- Ograniczenia dla tabeli `zamowienia`
+--
+ALTER TABLE `zamowienia`
+  ADD CONSTRAINT `zamowienia_ibfk_1` FOREIGN KEY (`id_dostawcy`) REFERENCES `dostawcy` (`id_dostawcy`),
+  ADD CONSTRAINT `zamowienia_ibfk_2` FOREIGN KEY (`id_klienta`) REFERENCES `klienci` (`id_klienta`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
