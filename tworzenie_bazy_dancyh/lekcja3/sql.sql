bcac14347f74c1258537a2ab77835dfb10245001
stepien20 <102522088+stepien20@users.noreply.github.com>
2024-03-01 11:04:23 +0100

    Update sql.sql

--- tworzenie_bazy_dancyh/lekcja3/sql.sql
+++ tworzenie_bazy_dancyh/lekcja3/sql.sql
@@ -1,1 +1,9 @@
-
+DELIMITER $$
+CREATE TRIGGER aktualizacjaSprzedazy BEFORE UPDATE on sprzedaz 
+for EACH ROW 
+BEGIN
+if (SELECT blokada FROM sprzedaz where id=NEW.id)>0 THEN
+SIGNAL SQLSTATE '45000' set message_text='Blad: nie mozna zaktualizowac rekordu';
+end IF;
+END$$
+DELIMITER ;
